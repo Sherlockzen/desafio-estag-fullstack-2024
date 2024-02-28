865856adff15d08beda8e3ee3459679a391380b2
Hewerton <hewertonlourenco@gmail.com>
2024-02-27 21:37:51 -0300

    refactor: improving banner style

--- front/src/components/banner.tsx
+++ front/src/components/banner.tsx
@@ -3,44 +3,46 @@ import Image from "next/image";
 
 function Banner() {
  return (
-  <div className=" h-[6.25rem] bg-[#8556AA] flex gap-8 py-6 pl-[15.5rem] text-white">
-   <div className=" flex gap-3 items-center">
-    <Image
-     src={"/dribble-square.svg"}
-     width={50}
-     height={52}
-     alt="dribble square icon"
-    />
-    <div>
-     <div className=" mb-3 font-bold">Tipo de quadra</div>
-     <div className=" font-light">Society</div>
+  <section className=" h-[6.25rem] bg-[#8556AA] flex justify-center gap-8 py-6 text-white">
+   <div className=" w-[54.375rem] flex gap-8">
+    <div className=" flex gap-3 items-center">
+     <Image
+      src={"/dribble-square.svg"}
+      width={50}
+      height={52}
+      alt="dribble square icon"
+     />
+     <div>
+      <div className=" mb-3 font-bold">Tipo de quadra</div>
+      <div className=" font-light">Society</div>
+     </div>
+    </div>
+    <div className=" flex gap-3 items-center">
+     <Image
+      src={"/align-left.svg"}
+      width={50}
+      height={52}
+      alt="dribble square icon"
+     />
+     <div>
+      <div className=" mb-3 font-bold">Nível</div>
+      <div className=" font-light">Semi-Profissional</div>
+     </div>
+    </div>
+    <div className=" flex gap-3 items-center">
+     <Image
+      src={"/trophy.svg"}
+      width={50}
+      height={52}
+      alt="dribble square icon"
+     />
+     <div>
+      <div className=" mb-3 font-bold">Vitórias</div>
+      <div className=" font-light">345</div>
+     </div>
     </div>
    </div>
-   <div className=" flex gap-3 items-center">
-    <Image
-     src={"/align-left.svg"}
-     width={50}
-     height={52}
-     alt="dribble square icon"
-    />
-    <div>
-     <div className=" mb-3 font-bold">Nível</div>
-     <div className=" font-light">Semi-Profissional</div>
-    </div>
-   </div>
-   <div className=" flex gap-3 items-center">
-    <Image
-     src={"/trophy.svg"}
-     width={50}
-     height={52}
-     alt="dribble square icon"
-    />
-    <div>
-     <div className=" mb-3 font-bold">Vitórias</div>
-     <div className=" font-light">345</div>
-    </div>
-   </div>
-  </div>
+  </section>
  );
 }
 
