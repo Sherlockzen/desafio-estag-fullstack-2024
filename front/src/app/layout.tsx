41973f3e67f6256bc02ada774b7e0f7231c880de
Hewerton <hewertonlourenco@gmail.com>
2024-02-28 17:31:26 -0300

    refactor: adjust layout

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -3,7 +3,10 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 
-const roboto = Roboto({ subsets: ["latin"], weight: ["300", "400", "700"] });
+const roboto = Roboto({
+ subsets: ["latin"],
+ weight: ["300", "500", "400", "700"],
+});
 
 export const metadata: Metadata = {
  title: "Treinador de Futebol",
@@ -19,7 +22,8 @@ export default function RootLayout({
   <html lang="pt-br">
    <body className={roboto.className}>
     <Header />
-    {children}
+    <div className=" pt-[10rem]">{children}</div>
+    <footer className=" h-[5.4375rem] bg-[#3D3D3D]"></footer>
    </body>
   </html>
  );
