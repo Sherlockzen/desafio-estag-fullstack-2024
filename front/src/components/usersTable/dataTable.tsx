29d494d653e4544533fe506ad0c085bcca1e757a
Hewerton <hewertonlourenco@gmail.com>
2024-02-29 11:29:09 -0300

    refactor: improving pagination

--- front/src/components/usersTable/dataTable.tsx
+++ front/src/components/usersTable/dataTable.tsx
@@ -47,7 +47,7 @@ export function DataTable<TData, TValue>({
   onGlobalFilterChange: setGlobalFilter,
  });
 
- function gerenatePages(): number[] {
+ function generatePages(): number[] {
   const pages = Array.from({ length: table.getPageCount() }, (_, i) => i + 1);
 
   return pages;
@@ -98,6 +98,7 @@ export function DataTable<TData, TValue>({
         onMouseLeave={() => row.toggleSelected(false)}
         key={row.id}
         data-state={row.getIsSelected() && "selected"}
+        className=" text-[#6A6A6A]"
        >
         {row.getVisibleCells().map((cell) => (
          <TableCell key={cell.id}>
@@ -115,20 +116,82 @@ export function DataTable<TData, TValue>({
      )}
     </TableBody>
    </Table>
-   <div className=" text-[#707070] w-full flex justify-between gap-[0.90625rem] -mt-4">
-    <div></div>
-    <div>
+   <div className=" text-[#707070] w-full flex items-center justify-between gap-[0.90625rem] -mt-4">
+    <div className=" uppercase font-medium text-xs">
+     Total <span className=" text-sm">{data.length}</span>
+    </div>
+    <div className=" w-96 flex items-center gap-4">
      <Button
       variant={"outline"}
-      className=" rounded-[1.125rem] uppercase bg-transparent hover:bg-[#9E9E9E]"
+      className=" rounded-[1.125rem] uppercase bg-transparent hover:bg-[#9E9E9E] border border-[#9E9E9E] text-xs font-medium"
       onClick={() => table.previousPage()}
+      disabled={!table.getCanPreviousPage()}
      >
       Anterior
      </Button>
+     <div className=" flex">
+      <Button
+       variant={"outline"}
+       className=" rounded-full uppercase bg-transparent hover:bg-[#9E9E9E] w-9 h-9 border border-[#9E9E9E]"
+       onClick={() => table.setPageIndex(0)}
+       disabled={!table.getCanPreviousPage()}
+      >
+       1
+      </Button>
+      <div
+       className={`flex items-center ${
+        table.getCanPreviousPage() ? "block" : "hidden"
+       }`}
+      >
+       ...
+      </div>
+      <Button
+       variant={"outline"}
+       className={` rounded-l-[1.125rem] rounded-r-none uppercase bg-transparent w-9 h-9 hover:bg-[#9E9E9E] border border-[#9E9E9E] ${
+        table.getCanPreviousPage() ? "block" : "hidden"
+       }`}
+       disabled={!table.getCanPreviousPage()}
+       onClick={() => table.previousPage()}
+      >
+       {table.getState().pagination.pageIndex}
+      </Button>
+      <Button
+       variant={"outline"}
+       className=" rounded-none uppercase bg-[#9E9E9E] text-white hover:bg-[#9E9E9E] w-9 h-9 hover:text-white border border-[#9E9E9E]"
+      >
+       {table.getState().pagination.pageIndex + 1}
+      </Button>
+      <Button
+       variant={"outline"}
+       className={` rounded-r-[1.125rem] rounded-l-none uppercase bg-transparent w-9 h-9 hover:bg-[#9E9E9E] border border-[#9E9E9E] ${
+        table.getCanNextPage() ? "block" : "hidden"
+       }`}
+       disabled={!table.getCanNextPage()}
+       onClick={() => table.nextPage()}
+      >
+       {table.getState().pagination.pageIndex + 2}
+      </Button>
+      <div
+       className={`flex items-center ${
+        table.getCanNextPage() ? "block" : "hidden"
+       }`}
+      >
+       ...
+      </div>
+      <Button
+       variant={"outline"}
+       className=" rounded-full uppercase bg-transparent hover:bg-[#9E9E9E] w-9 h-9 border border-[#9E9E9E]"
+       onClick={() => table.setPageIndex(generatePages().length - 1)}
+       disabled={!table.getCanNextPage()}
+      >
+       {generatePages().length}
+      </Button>
+     </div>
      <Button
       variant={"outline"}
-      className=" rounded-[1.125rem] uppercase bg-transparent hover:bg-[#9E9E9E]"
+      className=" rounded-[1.125rem] uppercase bg-transparent hover:bg-[#9E9E9E] border border-[#9E9E9E] text-xs font-medium"
       onClick={() => table.nextPage()}
+      disabled={!table.getCanNextPage()}
      >
       Próximo
      </Button>
@@ -142,7 +205,7 @@ export function DataTable<TData, TValue>({
       className=" w-10 bg-transparent border-b"
       value={table.getState().pagination.pageIndex + 1}
      >
-      {gerenatePages().map((page) => (
+      {generatePages().map((page) => (
        <option key={page} value={page} className=" text-sm">
         {page}
        </option>
